ab7b6bf89244b86de1d29f2f3d505b58124a4374
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2017-02-22 09:46:00 +0100

    MMT: update docstring

--- gpxity/backends/mmt.py
+++ gpxity/backends/mmt.py
@@ -7,28 +7,27 @@
 """
 This implements :class:`gpxity.backends.MMT` for http://www.mapmytracks.com
 
-There are many problems with the server running at mapmytracks.com:
-    * has problems with character sets, see MMT.__post
+There are some problems with the server running at mapmytracks.com:
     * it is not possible to change an existing activity - if the track changes, the
       activity must be re-uploaded and gets a new activity id This invalididates
       references held by other backend instances (maybe on other clients).
-    * does not support GPX very well. One problem is that it does not support gpx.time,
-      it ignores it in uploads and uses the time of the earliest trackpoint.
-    * do not know yet - does it support multiple tracks, multiple segments, waypoints?
-      I doubt it.
+      But I could imagine that most similar services have this problem too.
+    * does not support GPX very well beyond track data. One problem is that
+      it does not support gpx.time, it ignores it in uploads and uses the time
+      of the earliest trackpoint. To be consistent, Gpxity follows that for now
+      and does not respect gpx.time either.
     * there is an official description of an API at https://github.com/MapMyTracks
       but this does not implement everything needed. For the missing parts we
-      simulate what a web browser would do, see MMT.load_full(). Of course that
-      could fail if MMT changes its site. Which is true for the api itself,it can
-      and does get incompatible changes at any time without notice to users or
-      deprecation periods.
+      simulate what a web browser would do, see :meth:`MMT.load_full` and
+      :meth:`MMT._write_attribute`. Of course that could fail if MMT changes its site.
+      Which is true for the api itself, it can and does get incompatible changes at
+      any time without notice to users or deprecation periods.
     * downloading activities with that abi is very slow and hangs forever for big activities
       (at least this was so in Feb 2017, maybe have to test again occasionally).
-    * abi: upload_activity does not handle charsets correctly. It must accept UTF-8
-      (see https://url.spec.whatwg.org/#urlencoded-parsing). However mapmytracks.com
-      only decodes correctly if the client encodes everything as ISO8859-1.
-    * not all parts of MMT data are supported like tags (would be nice to have) or
-      images.
+    * not all parts of MMT data are supported like tags (would be nice to have.
+    Download is implemented but no upload) or images (not interesting for me,
+    at least not now).
+
 """
 
 from xml.etree import ElementTree
