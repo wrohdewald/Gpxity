a489ab63eb704f9f0e0ebc2a6575f88f05481af1
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2017-04-21 21:01:13 +0200

    make class BackendDiffSide local to class BackendDiff

--- gpxity/backend.py
+++ gpxity/backend.py
@@ -19,65 +19,66 @@ from .auth import Authenticate
 __all__ = ['Backend', 'BackendDiff']
 
 
-class BackendDiffSide:
-    """Represents a backend in BackendDiff.
-    """
-
-    # pylint: disable=too-few-public-methods
-
-    def __init__(self, backend, key_lambda=None):
-        self.backend = backend
-        self.key_lambda = key_lambda
-        self.entries = defaultdict(list)
-        for _ in backend:
-            try:
-                key = key_lambda(_)
-            except TypeError:
-                print('BackendDiffSide cannot apply key in {}: {}'.format(backend, _))
-                key = None
-            self.entries[key].append(_)
-        self.keys = set(self.entries.keys())
-        self.exclusive = dict()
-
-    def _use_other(self, other):
-        """use data from the other side"""
-        for _ in self.entries.keys():
-            if _ not in other.entries:
-                self.exclusive[_] = self.entries[_]
-
 class BackendDiff:
     """Compares two backends.directory
 
     Args:
         left (Backend): A backend
         right (Backend): The other one
-        key: A lambda which does the comparison. To be used like in sorted(list, key=...)
-            Default is the start time.
+        key: A lambda which does the comparison.
+            Default is the start time: `key=lambda x: x.time`
         key_right: Default is key. If given, this will be used for activities from right.
-            This allows things like BackendDiff(b1, b2, key_right = lambda x: x.time + hours2)
+            This allows things like `BackendDiff(b1, b2, key_right = lambda x: x.time + hours2)`
             where hours2 is a timedelta of two hours. If your GPX data has a problem with
             the time zone, this lets you find activities differring only by exactly 2 hours.
 
     Attributes:
-        times_left (set(datetime)): All start times from left
-        times_right (set(datetime)): All start times from right
-        times_left_only (set(datetime)): Start times which are not in right
-        times_right_only (set(datetime)): Start times which are not in left
-        activities_left_only list(Activity): Activities which are not in right
-        activities_right_only list(Activity): Activities which are not in left
-        activities_left_different list(Activity): Activities from left which are different in right
-        activities_right_different list(Activity): Activities from right which are different in left
+        left(:class:`BackendDiffSide`): Attributes for the left side
+        right(:class:`BackendDiffSide`): Attributes for the right side
+        keys_in_both(list): keys appearing on both sides.
+        matches(list): For every keys_in_both, this lists all matching activities from both sides
     """
 
     # pylint: disable=too-few-public-methods
+
+    class BackendDiffSide:
+        """Represents a backend in BackendDiff.
+
+        Attributes:
+            backend: The backend
+            key_lambda: The used lambda for calculating the key values
+            entries(dict): keys are what key_lambda calculates. values are lists of matching activities
+            exclusive(dict): keys with corresponding activity lists for activities existing only on this side
+        """
+
+        # pylint: disable=too-few-public-methods
+
+        def __init__(self, backend, key_lambda=None):
+            self.backend = backend
+            self.key_lambda = key_lambda
+            self.entries = defaultdict(list)
+            for _ in backend:
+                try:
+                    key = key_lambda(_)
+                except TypeError:
+                    print('BackendDiffSide cannot apply key in {}: {}'.format(backend, _))
+                    key = None
+                self.entries[key].append(_)
+            self.exclusive = dict()
+
+        def _use_other(self, other):
+            """use data from the other side"""
+            for _ in self.entries.keys():
+                if _ not in other.entries:
+                    self.exclusive[_] = self.entries[_]
 
     def __init__(self, left, right, key=None, right_key=None):
         if key is None:
             key = lambda x: x.time
         if right_key is None:
             right_key = key
-        self.left = BackendDiffSide(left, key)
-        self.right = BackendDiffSide(right, right_key)
+        self.left = BackendDiff.BackendDiffSide(left, key)
+        self.right = BackendDiff.BackendDiffSide(right, right_key)
         self.left._use_other(self.right) # pylint: disable=protected-access
         self.right._use_other(self.left) # pylint: disable=protected-access
         self.keys_in_both = self.left.entries.keys() & self.right.entries.keys()
