1fa1ba15342c4a7ed232747466cb9b0e4c91b92a
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2018-04-12 07:54:43 +0200

    Directory: do not print() when removing a dead symbolic link

--- gpxity/backends/directory.py
+++ gpxity/backends/directory.py
@@ -113,7 +113,6 @@ class Directory(Backend):
                             self._symlinks[gpx_target].append(full_name)
                     else:
                         os.remove(full_name)
-                        print('{}: removed dead symbolic link {}'.format(self, full_name))
 
     def _set_new_id(self, activity):
         """a not yet existant file name"""
