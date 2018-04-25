75f64f78501ff22c81065ae111462422fd8b3116
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2018-04-25 06:53:44 +0200

    setup.py: build long_description from README.rst and CHANGELOG.rst

--- setup.py
+++ setup.py
@@ -6,31 +6,17 @@ see https://setuptools.readthedocs.io/en/latest/setuptools.html
 
 from setuptools import setup, find_packages
 
-long_description = """
-Gpxity is a Python - library making it easy to move activities between different backends.
-In this context, a backend is a place where activities can be stored.
-
-Implemented backends are:
-
-  * Directory          for .gpx files on an accessible file system
-  * ServerDirectory    suited for a server implementation
-  * MMT                for activities on http://mapmytracks.com
-  * GPSIES             for activities on https://gpsies.com
-
-Sometimes you might just change a harmless thing like the description but
-the backend does not allow changing this separately, so we have to re-upload
-the whole activity. If it is is big and the remote server slow, this might
-take 10 minutes or more. Right now this library has no asynchronous interface,
-so it can really take some time until your program continues.
-
-"""
+def readall(path):
+    """explicitly close the file again"""
+    with open(path) as in_file:
+        return in_file.read()
 
 setup(
     name='Gpxity',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     description='A uniform interface to GPX services like mapmytracks or gpsies',
-    long_description=long_description,
+    long_description=readall('README.rst') + '\n\n' + readall('CHANGELOG.rst'),
     url='https://github.com/wrohdewald/Gpxity',
     author='Wolfgang Rohdewald',
     author_email='wolfgang@rohdewald.de',
