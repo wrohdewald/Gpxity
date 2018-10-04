a7484053341c419c7103761f4d9ca7c5a074f489
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2018-10-04 16:17:58 +0200

    release 1.3.3.2 adding gpxity/version.py

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+1.3.3.2 release 2018-10-04
+--------------------------
+
+  * fix creating doc at readthedocs.io
+
 1.3.3.1 release 2018-10-04
 --------------------------
 
---
+++ gpxity/version.py
@@ -0,0 +1,10 @@
+
+"""
+This file is created by bin/gittag
+"""
+
+VERSION = "1.3.3.2"
+COPYRIGHT = """
+Copyright (c) 2018 Wolfgang Rohdewald <wolfgang@rohdewald.de>
+See LICENSE for details.
+"""
