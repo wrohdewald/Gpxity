aae0d5c6e59213dc51dfe5a75997e68d88fb216e
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2018-10-22 13:21:00 +0200

    setup.py: separate extra_requires for develop and doc

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
     },
     extras_require={
         'WPTrackserver': ['mysqlclient'],
-        'develop': ['coverage', 'sphinx', 'sphinx-autodoc-annotation', 'sphinx-argparse', 'pytest', 'aiosmtpd'],
+        'develop': ['coverage', 'pytest', 'aiosmtpd'],
+        'doc': ['sphinx', 'sphinx-autodoc-annotation', 'sphinx-argparse']
     }
 )
