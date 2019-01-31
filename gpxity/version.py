d9295abdc68176fbad3835c1a32377f2ef04651b
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2019-01-31 19:22:10 +0100

    release 1.6.4

--- gpxity/version.py
+++ gpxity/version.py
@@ -3,7 +3,7 @@
 This file is created by bin/gittag
 """
 
-VERSION = "1.6.3"
+VERSION = "1.6.4"
 COPYRIGHT = """
 Copyright (c) 2019 Wolfgang Rohdewald <wolfgang@rohdewald.de>
 See LICENSE for details.
