5e3ebff34172dd062c26a1343433b3c133661e5d
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2020-01-10 10:45:53 +0100

    release 1.7.2

--- gpxity/version.py
+++ gpxity/version.py
@@ -3,7 +3,7 @@
 This file is created by bin/gittag
 """
 
-VERSION = "1.7.1"
+VERSION = "1.7.2"
 COPYRIGHT = """
 Copyright (c) 2019 Wolfgang Rohdewald <wolfgang@rohdewald.de>
 See LICENSE for details.
