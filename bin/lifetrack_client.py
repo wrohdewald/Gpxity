634dc827beaf0dadc0ec4fd2d630d8bd8c771858
Wolfgang Rohdewald <wolfgang@rohdewald.de>
2020-01-10 10:36:15 +0100

    updated lifetrack_client to current gpxity library and allow using a GPS mouse

--- bin/lifetrack_client.py
+++ bin/lifetrack_client.py
@@ -10,14 +10,18 @@
 # or see https://argcomplete.readthedocs.io/en/latest/
 
 
-"""lifetrack_client is a command line tool for lifetrack testing, client side."""
+"""lifetrack_client is a command line tool for lifetrack testing, client side.
+
+It can also use a GPS mouse as source."""
 
 import argparse
 import os
 import sys
 import logging
+import datetime
 import time
 import random
+import serial
 
 try:
     import argcomplete
@@ -25,9 +29,17 @@ try:
 except ImportError:
     pass
 
+try:
+    from pynmea2 import parse as parse_nmea
+    from pynmea2.nmea import ParseError
+    HAVE_NMEA = True
+except ImportError:
+    HAVE_NMEA = False
+
 from gpxpy import gpx as mod_gpx
 GPXTrack = mod_gpx.GPXTrack
 GPXTrackSegment = mod_gpx.GPXTrackSegment
+GPXTrackPoint = mod_gpx.GPXTrackPoint
 
 
 # This uses not the installed copy but the development files
@@ -37,6 +49,56 @@ if os.path.exists(os.path.join(_, 'gpxity', '__init__.py')):
 # pylint: disable=wrong-import-position
 from gpxity import GpxFile, Lifetrack, Backend # noqa
 
+class Source:
+
+    """Abstract API for either GPX file or a GPS mouse."""
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self, name):
+        self.name = name
+        if name.startswith('/dev/'):
+            if not HAVE_NMEA:
+                print('The python library pynmea2 is not available')
+                sys.exit(2)
+            self.__source = serial.Serial(name)
+        else:
+            self.__source = Backend.instantiate(name)
+            assert isinstance(self.__source, GpxFile)
+
+    def points(self):
+        """Return the points."""
+        altitude = None
+        if isinstance(self.__source, serial.Serial):
+            visible_sats = {}
+            while True:
+                raw = self.__source.readline().decode()
+                provider = raw[1:3]
+                try:
+                    msg = parse_nmea(raw)
+                    if hasattr(msg, 'altitude'):
+                        altitude = msg.altitude
+                    if msg.sentence_type == 'RMC':
+                        if msg.is_valid:
+                            logging.debug('RMC: %s', msg)
+                            yield  GPXTrackPoint(
+                                latitude=msg.latitude,
+                                longitude=msg.longitude,
+                                time=msg.datetime.replace(
+                                    microsecond=0, tzinfo=datetime.timezone.utc),
+                                elevation=altitude)
+                        else:
+                            logging.debug('INVALID RMC: %s', msg)
+                    elif msg.sentence_type == 'GSV' and int(msg.msg_num) == 1:
+                        if visible_sats.get(provider) != msg.num_sv_in_view:
+                            visible_sats[provider] = msg.num_sv_in_view
+                            logging.debug('Visible satellites: %s', visible_sats)
+                except ParseError:
+                    pass
+        else:
+            for _ in self.__source.points():
+                time.sleep(random.randrange(10))
+                yield _
 
 class Main:
 
@@ -55,17 +117,22 @@ class Main:
         self.logger.setLevel(self.options.loglevel.upper())
         self.source = None
         try:
-            source = Backend.instantiate(self.options.source)
-            assert isinstance(source, GpxFile)
-            backend = Backend.instantiate(self.options.backend)
-            assert isinstance(backend, Backend)
-            life = Lifetrack('127.0.0.1', [backend])
-            all_points = list(source.points())
-            life.start(all_points[:5])
-            for point in all_points[5:]:
-                time.sleep(random.randrange(10))
-                life.update_trackers([point])
-            time.sleep(random.randrange(10))
+            source = Source(self.options.source)
+            backends = []
+            tracker_id = None
+            for _ in self.options.backend:
+                backend = Backend.instantiate(_)
+                if not isinstance(backend, Backend):
+                    tracker_id = backend.id_in_backend
+                    backend = backend.backend
+                backends.append(backend)
+            life = None
+            for point in source.points():
+                if life is None:
+                    life = Lifetrack('127.0.0.1', backends, tracker_id=tracker_id)
+                    life.start([point])
+                else:
+                    life.update_trackers([point])
             life.end()
         except Exception as _:  # pylint: disable=broad-except
             self.error(_)
@@ -83,8 +150,8 @@ class Main:
         """into self.options."""
         # pylint: disable=too-many-statements, too-many-branches
         parser = argparse.ArgumentParser('lifetrack_client')
-        parser.add_argument('--source', help='the gpxfile with test data')
-        parser.add_argument('--backend', help='the server')
+        parser.add_argument('--source', help='the gpxfile with test data', required=True)
+        parser.add_argument('--backend', help='the server', required=True, action='append')
         parser.add_argument(
             '--loglevel', help='set the loglevel',
             choices=('debug', 'info', 'warning', 'error'), default='error')
